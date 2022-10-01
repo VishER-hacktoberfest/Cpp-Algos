0fcbd51d015572bae1401dc892588bcb6738d23a
Ananya Mazumder <ananya1562000@gmail.com>
2022-10-02 02:12:01 +0530

    Cpp solution for linear search added.

---
+++ Searching/linear-search.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+ 
+int search(int arr[], int n, int key)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        if (arr[i] == key)
+            return i;
+    return -1;
+}
+ 
+// Driver's code
+int main(void)
+{
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int key = 10;
+    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    // Function call
+    int result = search(arr, n, key);
+    (result == -1)
+        ? cout << "Element is not present in array"
+        : cout << "Element is present at index " << result;
+    return 0;
+}
